0b7695cc3e8297c457d1809c1afdf371bc0ac44b
Gabriel Sobral <asgoth55@gmail.com>
2021-04-19 22:35:25 -0300

    initial commit - first day

--- App.tsx
+++ App.tsx
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StatusBar } from 'react-native'
+import { Welcome } from './src/pages/Welcome'
+import colors from './src/styles/colors'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  return(
+    <>
+      <StatusBar 
+        translucent={true}
+        animated={true}
+        barStyle="dark-content"
+        backgroundColor= { colors.background }
+        showHideTransition="slide"
+      />
+      <Welcome />
+    </>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
